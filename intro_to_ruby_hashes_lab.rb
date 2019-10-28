a243fedee1dbb8e708cc4211d42548599c77e1f8
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-28 22:10:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly_with_third_tier
               {:pieces=> 4, :rent_in_dollars=>{
                 :one_piece_owned=>25,
                 :two_pieces_owned=>50,
-                :three=>"",
+                :three_pieces_owned=>100,
                 :four=>"" 
                 }, :names=>{
                   :five=>"",
